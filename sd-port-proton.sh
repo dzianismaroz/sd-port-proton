a7ae62210f29e2867175068b47f6c9219ac88f22
dzianismaroz <43498404+dzianismaroz@users.noreply.github.com>
2024-01-22 22:05:31 +0300

    Create sd-port-proton.sh

---
+++ sd-port-proton.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear 
+
+steamos-readonly disable
+
+echo "Init and populating pacman keys"
+pacman-key --init  >> /dev/null
+pacman-key -u >> /dev/null
+pacman-key --populate >> /dev/null
+
+echo "Installing cabextract and required dependencies"
+
+pacman -S --noconfirm cabextract
+pacman -Syu --noconfirm bash icoutils wget bubblewrap zstd bc tar openssl \
+gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font zenity \
+lsb-release  nss xorg-xrandr vulkan-driver vulkan-icd-loader lsof lib32-freetype2 \
+lib32-libgl lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins \
+lib32-libgpg-error lib32-nss lib32-vulkan-driver lib32-vulkan-icd-loader \ 
+lib32-gamemode lib32-openssl >> /dev/null
+
+echo "Download and install PortProton"
+
+wget -c "https://github.com/Castro-Fidel/PortWINE/raw/master/portwine_install_script/PortProton_1.0" && sh PortProton_1.0 
+
+echo "Resore SteamOS redad-only mode"
+
+steamos-readonly enable
+
+echo "Good game"
