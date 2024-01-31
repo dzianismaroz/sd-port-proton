eddc21f74f54c6a425648332888bb9a4c6e9d909
dzianismaroz <43498404+dzianismaroz@users.noreply.github.com>
2024-01-31 18:54:47 +0300

    log file to collect errors

--- sd-port-proton.sh
+++ sd-port-proton.sh
@@ -2,6 +2,8 @@
 
 clear
 
+exec 2> errors.log
+
 echo -e '################################################'
 echo -e '  Install all dependencies for PortProton 1.0'
 echo -e '################################################'
@@ -13,20 +15,20 @@ echo -e 'Please provide your sudo password'
 steamos-readonly disable &> /dev/null
 
 echo "Init and populating pacman keys"
-pacman-key --init
-pacman-key -u
-pacman-key --populate
+pacman-key --init  1> /dev/null
+pacman-key -u 1> /dev/null
+pacman-key --populate 1> /dev/null
 
 echo "Installing cabextract and required dependencies"
 
-pacman -Syu --noconfirm cabextract
+pacman -Syu --noconfirm cabextract 1> /dev/null
 pacman -S --noconfirm bash icoutils wget bubblewrap zstd bc tar openssl \
-gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font
-pacman -S --noconfirm zenity-light
+gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font 1> /dev/null
+pacman -S --noconfirm zenity-light 1> /dev/null
 pacman -S --noconfirm lsb-release  nss xorg-xrandr vulkan-driver vulkan-icd-loader \
 lsof lib32-freetype2 lib32-libgl lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins \
 lib32-libgpg-error lib32-nss lib32-vulkan-driver lib32-vulkan-icd-loader \
-lib32-gamemode lib32-openssl
+lib32-gamemode lib32-openssl 1> /dev/null
 
 echo "Restore SteamOS read-only mode"
 
@@ -38,5 +40,3 @@ echo "Done"
 echo "Download and install PortProton v 1.0"
 
 wget -cq "https://github.com/Castro-Fidel/PortWINE/raw/master/portwine_install_script/PortProton_1.0" && sudo -u deck bash ./PortProton_1.0
-
-
