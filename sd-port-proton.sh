16627e74569e5fab12c1ce177140aa23d931ab61
dzianismaroz <43498404+dzianismaroz@users.noreply.github.com>
2024-02-03 20:55:28 +0300

    Update sd-port-proton.sh

--- sd-port-proton.sh
+++ sd-port-proton.sh
@@ -22,10 +22,11 @@ pacman-key --populate 1> /dev/null
 echo "Installing cabextract and required dependencies"
 
 pacman -Syu --noconfirm cabextract 1> /dev/null
-pacman -S --noconfirm bash icoutils wget bubblewrap zstd bc tar openssl \
+pacman -Syu --noconfirm bash icoutils wget bubblewrap zstd bc tar openssl \
 gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font 1> /dev/null
-pacman -S --noconfirm zenity-light 1> /dev/null
-pacman -S --noconfirm lsb-release  nss xorg-xrandr vulkan-driver vulkan-icd-loader \
+pacman -Syu --noconfirm zenity 1> /dev/null
+pacman -Syu --noconfirm zenity-light 1> /dev/null
+pacman -Syu --noconfirm lsb-release  nss xorg-xrandr vulkan-driver vulkan-icd-loader \
 lsof lib32-freetype2 lib32-libgl lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins \
 lib32-libgpg-error lib32-nss lib32-vulkan-driver lib32-vulkan-icd-loader \
 lib32-gamemode lib32-openssl 1> /dev/null
