f2ef5908f8bc4190fb23d4b68bd0094cbcdb9862
dzianismaroz <43498404+dzianismaroz@users.noreply.github.com>
2024-02-06 15:08:13 +0300

    from Fidels
    use patch vesion with noisy output

--- sd-port-proton.sh
+++ sd-port-proton.sh
@@ -2,8 +2,6 @@
 
 clear
 
-exec 2> errors.log
-
 echo -e '################################################'
 echo -e '  Install all dependencies for PortProton 1.0'
 echo -e '################################################'
@@ -15,21 +13,20 @@ echo -e 'Please provide your sudo password'
 steamos-readonly disable &> /dev/null
 
 echo "Init and populating pacman keys"
-pacman-key --init  1> /dev/null
-pacman-key -u 1> /dev/null
-pacman-key --populate 1> /dev/null
+pacman-key --init
+pacman-key -u
+pacman-key --populate
 
 echo "Installing cabextract and required dependencies"
 
-pacman -Syu --noconfirm cabextract 1> /dev/null
-pacman -Syu --noconfirm bash icoutils wget bubblewrap zstd bc tar openssl \
-gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font 1> /dev/null
-pacman -Syu --noconfirm zenity 1> /dev/null
-pacman -Syu --noconfirm zenity-light 1> /dev/null
-pacman -Syu --noconfirm lsb-release  nss xorg-xrandr vulkan-driver vulkan-icd-loader \
+pacman -Syu --noconfirm cabextract
+pacman -S --noconfirm bash icoutils wget bubblewrap zstd bc tar openssl \
+gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font
+pacman -S --noconfirm zenity-light
+pacman -S --noconfirm lsb-release  nss xorg-xrandr vulkan-driver vulkan-icd-loader \
 lsof lib32-freetype2 lib32-libgl lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins \
 lib32-libgpg-error lib32-nss lib32-vulkan-driver lib32-vulkan-icd-loader \
-lib32-gamemode lib32-openssl 1> /dev/null
+lib32-gamemode lib32-openssl
 
 echo "Restore SteamOS read-only mode"
 
